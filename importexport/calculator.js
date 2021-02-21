64d11f53ff974ddca8eae07c99a7f12df8014a2e
waqas1ishaq <waqasishaq800@gmail.com>
2021-02-21 17:27:13 +0000

    clculator

--- importexport/calculator.js
+++ importexport/calculator.js
@@ -1,1 +1,18 @@
 console.log("welcome my simple calculator");
+const add=(a,b)=>{
+  return a+b;
+}
+
+const mul=(a,b)=>{
+  return a-b;
+}
+
+const subs=(a,b)=>{
+  return a*b;
+}
+
+const divide=(a,b)=>{
+  return a/b;
+}
+
+module.exports={add,mul,subs,divide};
---
+++ importexport/index.js
@@ -0,0 +1,5 @@
+const {add,subs,mul,divide}=require('./calculator');
+console.log(add(23,23));
+console.log(subs(23,23));
+console.log(divide(23,23));
+console.log(mul(23,23));
