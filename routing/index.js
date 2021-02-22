8f9763a8c65369b1080fc8df08c8c135fc22ccef
waqas1ishaq <waqasishaq800@gmail.com>
2021-02-22 17:59:21 +0000

    server

--- routing/index.js
+++ routing/index.js
@@ -1,2 +1,8 @@
 const http=require('http');
 console.log(http);
+http.createServer((req,res)=>{
+  req.end("welcome in first time server creation");
+});
+Server.listen(8000,"127.0.0.0",()=>{
+  console.log("we are created server");
+});
